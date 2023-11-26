b6172a2fe3a7eaa90059908ba31e9cb9d0060487
sohan-99 <sohan75632@gmail.com>
2023-11-26 23:50:23 +0600

    created API in Service Name in dataBase

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
-const port = process.env.PORT || 5000 ;
+const port = process.env.PORT || 5000;
 // middlewere
 app.use(cors());
 app.use(express.json());
@@ -22,23 +22,37 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const serviceCollation = client.db('employeeManagementDB').collection('servicetype');
+
+    // get all data to database 
+    app.get('/servicetype', async (req, res) => {
+      const result = await serviceCollation.find().toArray();
+      res.send(result)
+    })
+
+    // get data in service name 
+    app.get('/servicetype/:service_name', async (req, res) => {
+      const service_name = req.params.service_name
+      const query = { service_name: service_name }
+      const result = await serviceCollation.findOne(query);
+      res.send(result);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
 
-app.get('/',(req, res)=>{
-    res.send('coffe-store-server is runing')
+app.get('/', (req, res) => {
+  res.send('coffe-store-server is runing')
 })
 
-app.listen(port,()=>{
-    console.log(`this is port :${port}`);
+app.listen(port, () => {
+  console.log(`this is port :${port}`);
 })

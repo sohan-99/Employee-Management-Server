f2377307d719e71a940f22b4b9e2943fd684e020
sohan-99 <sohan75632@gmail.com>
2023-11-30 12:42:20 +0600

    Employee list in Hr role

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.julqny1.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -26,6 +25,29 @@ async function run() {
     await client.connect();
     const serviceCollection = client.db('employeeManagementDB').collection('servicetype');
     const usersCollection = client.db("employeeManagementDB").collection("users");
+
+     // jwt related api
+     app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ token });
+    })
+
+      // // middlewares 
+      // const verifyToken = (req, res, next) => {
+      //   console.log('inside verify token', req.headers.authorization);
+      //   if (!req.headers.authorization) {
+      //     return res.status(401).send({ message: 'unauthorized access' });
+      //   }
+      //   const token = req.headers.authorization.split(' ')[1];
+      //   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      //     if (err) {
+      //       return res.status(401).send({ message: 'unauthorized access' })
+      //     }
+      //     req.decoded = decoded;
+      //     next();
+      //   })
+      // }
 
     // get all data to database 
     app.get('/servicetype', async (req, res) => {
@@ -58,6 +80,7 @@ async function run() {
 
     // all employee get and show in website
     app.get('/users', async (req, res) => {
+      console.log(req.headers);
       const result = await usersCollection.find().toArray();
       res.send(result)
     })
@@ -83,7 +106,12 @@ async function run() {
         res.send(result);
       })
 
-      // 
+      //all employee list api  
+      app.get("/allemployee", async (req, res) => {
+        const filter = { designation: "Employee" };
+        const result = await usersCollection.find(filter).toArray();
+        res.send(result);
+      });
 
 
     // Send a ping to confirm a successful connection
